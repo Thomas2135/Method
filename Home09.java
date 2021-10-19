d51472e66500fc5b2e97c0534be95a688573a524
韬哥 <denniswang1998@gmail.com>
2021-10-19 15:51:11 +0800

    lll

---
+++ Home09.java
@@ -0,0 +1,7 @@
+class Home09{
+	public static void main(String[] args) {
+		System.out.println("ggggggggggggggg");
+		System.out.println("FFFFFFFFFFFFFF");
+		System.out.println("aaaaaaaaaaaaa");
+	}
+}
